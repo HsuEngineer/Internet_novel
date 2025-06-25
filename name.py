419ba35fbc63cdf0fcedbac423d06bf40ab923b0
Hsu Engineer <130962683+HsuEngineer@users.noreply.github.com>
2025-06-25 09:53:27 +0800

    Delete name.py

--- name.py
+++
@@ -1,8 +0,0 @@
-import os
-list = os.listdir('./content/轉生後想要在田園過慢生活/txt')
-# print(list[0].split()[1].split('.')[0])
-
-# for name in list:
-#     arr = name.split()
-#     os.rename('content/' + name, f'content/{int(arr[0]):03d} {arr[1].split('.')[0]}.txt')
-print(list)
